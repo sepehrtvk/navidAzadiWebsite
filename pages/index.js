a3b2540dbb4904e913a7bac9a7f4d790972de776
Sepehr Tavakoli <sepehrtvk2001@gmail.com>
2023-09-09 14:42:32 +0330

    add bootstrap

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import Head from "next/head";
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>Max' Blog</title>
+        <meta
+          name='description'
+          content='I post about programming and web development.'
+        />
+      </Head>
+      <h4>ff</h4>
+    </Fragment>
+  );
+}
+
+export default HomePage;
